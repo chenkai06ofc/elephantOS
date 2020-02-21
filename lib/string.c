935be02bb0532e178cfd531e26826038af21db3c
chen <chenkai06ofc@gmail.com>
2020-02-21 16:53:41 +0900

    add functions to string.c

--- lib/string.c
+++ lib/string.c
@@ -1,30 +1,70 @@
 #include "string.h"
 #include "stdint.h"
+#include "global.h"
 
-/** set size bytes from start addr to value */
-void memset1(void* start, uint8_t value, uint32_t size) {
-    uint8_t* start_ = (uint8_t*)start;
+/** set size bytes from start_ addr to value */
+void memset(void* start_, uint8_t value, uint32_t size) {
+    uint8_t* start = (uint8_t*)start_;
     int i = 0;
     for (i = 0; i < size; i++) {
-        *(start_ + i) = value;
+        *(start + i) = value;
     }
 }
 
-/** copy size bytes from src addr to dst */
-void memcpy1(void* dst, const void* src, uint32_t size) {
-    uint8_t* dst_ = (uint8_t*) dst;
-    const uint8_t* src_ = (uint8_t*) src;
+/** copy size bytes from src_ addr to dst_ */
+void memcpy(void* dst_, const void* src_, uint32_t size) {
+    uint8_t* dst = (uint8_t*) dst_;
+    const uint8_t* src = (uint8_t*) src_;
     int i;
-    if (dst_ == src_) {
+    if (dst == src) {
         return;
-    } else if (dst_ < src_) {
+    } else if (dst < src) {
         for (i = 0; i < size; i++) {
-            *(dst_ + i) = *(src_ + i);
+            *(dst + i) = *(src + i);
         }
     } else {
         for (i = size - 1; i >= 0; i--) {
-            *(dst_ + i) = *(src_ + i);
+            *(dst + i) = *(src + i);
         }
     }
 }
 
+/** compare size bytes start from a_ and b_, return 0: a_ = b_, 1: a_ > b_, -1: a_ < b_ */
+int memcmp(const void* a_, const void* b_, uint32_t size) {
+    const char* a = a_;
+    const char* b = b_;
+    char xa, xb;
+    while (size > 0) {
+        xa = *a;
+        xb = *b;
+        if (xa != xb) {
+            return xa > xb ? 1 : -1;
+        }
+        a++;
+        b++;
+        size--;
+    }
+    return 0;
+}
+
+/** copy string from src_ to dst_ */
+char* strcpy(char* dst_, const char* src_) {
+    char* r = dst_;
+    while (1) {
+        if (!(*dst_ = *src_)) {
+            break;
+        }
+        dst_++;
+        src_++;
+    }
+    return r;
+}
+
+/** length of string */
+uint32_t strlen(const char* str) {
+    const char* p = str;
+    while (*p) {
+        p++;
+    }
+    return (p - str);
+}
--- lib/string.h
+++ lib/string.h
@@ -1,6 +1,10 @@
 #ifndef __LIB_STRING_H
 #define __LIB_STRING_H
 #include "stdint.h"
-void memset1(void* start, uint8_t value, uint32_t size);
-void memcpy1(void* dst, const void* src, uint32_t size);
+void memset(void* start, uint8_t value, uint32_t size);
+void memcpy(void* dst, const void* src, uint32_t size);
+int memcmp(const void* a_, const void* b_, uint32_t size);
+
+char* strcpy(char* dst_, const char* src_);
+uint32_t strlen(const char* str);
 #endif //__LIB_STRING_H
