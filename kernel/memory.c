88c9fa9d4b2f1fdf69e16a7e83b7ee072f561b39
chen <chenkai06ofc@gmail.com>
2020-03-05 14:50:57 +0900

    add macro PHY_ADDR

--- kernel/memory.c
+++ kernel/memory.c
@@ -12,6 +12,11 @@
 
 #define PDE_IDX(addr)   ((addr & 0xffc00000) >> 22)
 #define PTE_IDX(addr)   ((addr & 0x003ff000) >> 12)
+/* addr is uint32_t, return uint32_t* */
+#define PDE_PTR(addr)   (uint32_t*)(0xfffff000 | ((addr & 0xffc00000) >> 20));
+#define PTE_PTR(addr)   (uint32_t*)(0xffc00000 | ((vaddr & 0xffc00000) >> 10) | ((vaddr & 0x003ff000) >> 10));
+/* get physical address from virtual address, vaddr is uint32_t, return uint32_t */
+#define PHY_ADDR(vaddr) (((*PTE_PTR(vaddr)) & 0xfffff000) | (vaddr & 0x00000fff))
 
 struct paddr_pool p_kernel_pool, p_user_pool;
 struct vaddr_pool v_kernel_pool;
@@ -129,19 +134,9 @@ static void* palloc(struct paddr_pool* pool_ptr) {
     }
 }
 
-/** get pte pointer of vaddr */
-uint32_t* get_pte_ptr(uint32_t vaddr) {
-    return (uint32_t*)(0xffc00000 | ((vaddr & 0xffc00000) >> 10) | ((vaddr & 0x003ff000) >> 10));
-}
-
-/** get pde pointer of vaddr */
-uint32_t* get_pde_ptr(uint32_t vaddr) {
-    return (uint32_t*)(0xfffff000 | (PDE_IDX(vaddr) * 4));
-}
-
 static void map_vaddr_paddr(uint32_t vaddr, uint32_t paddr) {
-    uint32_t* pde_ptr = get_pde_ptr(vaddr);
-    uint32_t* pte_ptr = get_pte_ptr(vaddr);
+    uint32_t* pde_ptr = PDE_PTR(vaddr);
+    uint32_t* pte_ptr = PTE_PTR(vaddr);
     // PDE not present
     if (!(*pde_ptr) & PG_P_1) {
         uint32_t pt_page_paddr = (uint32_t)palloc(&p_kernel_pool);
